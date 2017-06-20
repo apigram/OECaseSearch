79fd3c8f2abbcd62b64f08b08ffc6ae303958bfe
Andrew Pigram <andrew.pigram@fivium.com.au>
2017-06-20 17:17:42 +1000

    Added extra text to the name link to indicate if a patient has any unconfirmed diagnoses.

--- views/caseSearch/search_results.php
+++ views/caseSearch/search_results.php
@@ -6,7 +6,10 @@
 ?>
 
 <div class="result box generic">
-  <h3 class="box-title"><?php echo CHtml::link($data->contact->last_name . ', ' . $data->contact->first_name . ($data->is_deceased ? ' (Deceased)' : ''),
+  <h3 class="box-title"><?php echo CHtml::link($data->contact->last_name
+          . ', ' . $data->contact->first_name
+          . ($data->is_deceased ? ' (Deceased)' : '')
+          . ($data->hasUnconfirmedDiagnoses() ? ' (Unconfirmed)' : ''),
           array('/patient/view', 'id' => $data->id), array('target' => '_blank')); ?></h3>
   <div class="row data-row">
     <div class="large-12 column">

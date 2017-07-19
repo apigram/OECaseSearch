930a06ecf19249c98edc9e852ef59361a2f82a8e
Liam Marshall <marshl@live.com.au>
2017-07-19 15:37:17 +1000

    Removed code that is dependent on diagnosis confirmation

--- models/PatientDiagnosisParameter.php
+++ models/PatientDiagnosisParameter.php
@@ -6,10 +6,6 @@
 class PatientDiagnosisParameter extends CaseSearchParameter implements DBProviderInterface
 {
     public $textValue;
-    public $isConfirmed;
-
-    const DIAGNOSIS_CONFIRMED = 1;
-    const DIAGNOSIS_UNCONFIRMED = 0;
 
     /**
      * PatientAgeParameter constructor. This overrides the parent constructor so that the name can be immediately set.
@@ -33,7 +29,7 @@ class PatientDiagnosisParameter extends CaseSearchParameter implements DBProvide
      */
     public function attributeNames()
     {
-        return array_merge(parent::attributeNames(), array('textValue', 'isConfirmed'));
+        return array_merge(parent::attributeNames(), array('textValue'));
     }
 
     /**
@@ -44,7 +40,7 @@ class PatientDiagnosisParameter extends CaseSearchParameter implements DBProvide
     {
         return array_merge(parent::rules(), array(
                 array('textValue', 'required'),
-                array('textValue, isConfirmed', 'safe'),
+                array('textValue', 'safe'),
             )
         );
     }
@@ -57,10 +53,6 @@ class PatientDiagnosisParameter extends CaseSearchParameter implements DBProvide
             'NOT LIKE' => 'Not diagnosed with',
         );
 
-        $diagOptions = array(
-            self::DIAGNOSIS_UNCONFIRMED => 'Unconfirmed only',
-            self::DIAGNOSIS_CONFIRMED => 'Confirmed only',
-        );
         ?>
       <div class="large-1 column">
           <?php echo CHtml::label($this->getLabel(), false); ?>
@@ -86,10 +78,6 @@ class PatientDiagnosisParameter extends CaseSearchParameter implements DBProvide
           ?>
           <?php echo CHtml::error($this, "[$id]textValue"); ?>
       </div>
-      <div class="large-3 column">
-          <?php echo CHtml::activeDropDownList($this, "[$id]isConfirmed", $diagOptions,
-              array('empty' => 'Confirmed/Unconfirmed')); ?>
-      </div>
         <?php
     }
 
@@ -108,11 +96,7 @@ LEFT JOIN secondary_diagnosis sd
 LEFT JOIN disorder d 
   ON d.id = sd.disorder_id 
 WHERE LOWER(d.term) LIKE LOWER(:p_d_value_$this->id)";
-        if ($this->isConfirmed === '0') {
-            $query .= " AND sd.is_confirmed = :p_d_confirmed_$this->id";
-        } elseif ($this->isConfirmed === '1') {
-            $query .= " AND ((:p_d_confirmed_$this->id = " . self::DIAGNOSIS_CONFIRMED . " AND sd.is_confirmed IS NULL) OR sd.is_confirmed = :p_d_confirmed_$this->id)";
-        }
+
         switch ($this->operation) {
             case 'LIKE':
                 // Do nothing extra.
@@ -138,14 +122,6 @@ WHERE p1.id NOT IN (
      */
     public function bindValues()
     {
-        // Construct your list of bind values here. Use the format ":bind" => "value".
-        if ($this->isConfirmed !== '') {
-            return array(
-                "p_d_value_$this->id" => '%' . $this->textValue . '%',
-                "p_d_confirmed_$this->id" => $this->isConfirmed,
-            );
-        }
-
         return array(
             "p_d_value_$this->id" => '%' . $this->textValue . '%',
         );
